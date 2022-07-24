fdd9f6d1cbe9d0c96fdb2c2a788c2de8ad42103b
Ewelina Nieweglowska <nieweglowska.ewelina@gmail.com>
2022-07-24 12:24:34 +0200

    js & mobile menu optimisation

--- script.js
+++ script.js
@@ -1,14 +1,8 @@
 const button = document.getElementById("menu");
 const mobilenav = document.getElementById("mobileNav");
 
-mobilenav.style.display = "none";
-button.style.opacity = 1;
-
 button.onclick = function(){
-    if(mobilenav.style.display == "none"){
-        mobilenav.style.display = "block";
-        button.style.opacity = 0.5;
-    } else { 
-        mobilenav.style.display = "none";
-        button.style.opacity = 1}
+    button.classList.toggle("active");
+    mobilenav.classList.toggle("active");
 }
+
